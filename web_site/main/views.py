4bae3cc1f4eb4e9043a42fd35fd1f14aba51d837
Maks <20052601l@gmail.com>
2024-02-27 20:43:28 +0300

    дз новая страница

--- web_site/main/views.py
+++ web_site/main/views.py
@@ -4,8 +4,11 @@ from .models import Task
 
 
 def index(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.order_by('-id')
     return render(request, 'main/index.html', {'title': 'Главная страница', 'tasks':tasks})
 
 def about(request):
     return render(request, 'main/about.html')
+
+def create(request):
+    return render(request, 'main/create.html')
