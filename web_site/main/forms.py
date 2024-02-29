bad9c3f68671feb382caf37eb5250f7290dc53b7
Maks <20052601l@gmail.com>
2024-03-01 02:11:33 +0300

    дз шаблон полей в форме

--- web_site/main/forms.py
+++ web_site/main/forms.py
@@ -1,9 +1,9 @@
 from .models import Task
-from django.forms import ModelForm, TextInput, widgets
+from django.forms import ModelForm, TextInput, Textarea
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ["title","task"]
         widgets = {"title": TextInput(attrs={'class':'form-control', 'placeholder':'Введите название'}),
-                   "task": TextInput(attrs={'class': 'p-4 mb-3 bg-body-tertiary rounded', 'placeholder': 'Введите задачу'}) }
+                   "task": Textarea(attrs={'class': 'p-4 mb-3 bg-body-tertiary rounded', 'placeholder': 'Введите задачу'}) }
