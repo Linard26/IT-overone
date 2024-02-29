7cbb7396de8f1283256f8dcd02327ff534cf03b8
Maks <20052601l@gmail.com>
2024-02-29 03:38:28 +0300

    дз форма для поля

--- web_site/main/views.py
+++ web_site/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
 
 
 def index(request):
@@ -11,4 +12,8 @@ def about(request):
     return render(request, 'main/about.html')
 
 def create(request):
-    return render(request, 'main/create.html')
+    form = TaskForm()
+    context = {
+        'form':form
+    }
+    return render(request, 'main/create.html',context)
