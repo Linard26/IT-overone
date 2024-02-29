079b78e19052e4b53e190bcfbd3dcb4f8de7ea25
Maks <20052601l@gmail.com>
2024-02-29 04:01:20 +0300

    дз поле в форме

--- web_site/main/forms.py
+++ web_site/main/forms.py
@@ -1,8 +1,9 @@
 from .models import Task
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, widgets
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ["title","task"]
-
+        widgets = {"title": TextInput(attrs={'class':'form-control', 'placeholder':'Введите название'}),
+                   "task": TextInput(attrs={'class': 'p-4 mb-3 bg-body-tertiary rounded', 'placeholder': 'Введите задачу'}) }
